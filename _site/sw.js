f8c832c3f5cd39394aff1529fd392ff2093829ed
lee <lee@chaui-MacBookPro.local>
2023-12-19 22:02:23 +0900

    npm i && npm run build

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1702942282';
+const cacheName = 'chirpy-1702990711';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
