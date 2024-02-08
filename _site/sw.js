e90810f20811a820a904a43a5afbf482da67438d
lee <lee@chaui-MacBookPro.local>
2024-02-08 15:32:06 +0900

    post

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703070243';
+const cacheName = 'chirpy-1707373905';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
